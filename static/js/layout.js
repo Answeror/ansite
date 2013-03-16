efd7329a30d91b4c4df332c48dfeddb315fc9992
answeror <answeror@gmail.com>
2013-03-16 20:27:53 +0800

    highlight current page on nav bar

--- static/js/layout.js
+++ static/js/layout.js
@@ -1,15 +1,15 @@
-//first, checks if it isn't implemented yet
-if (!String.prototype.format) {
-  String.prototype.format = function() {
-    var args = arguments;
-    return this.replace(/{(\d+)}/g, function(match, number) { 
-      return typeof args[number] != 'undefined'
-        ? args[number]
-        : match
-      ;
-    });
-  };
-}
+////first, checks if it isn't implemented yet
+//if (!String.prototype.format) {
+  //String.prototype.format = function() {
+    //var args = arguments;
+    //return this.replace(/{(\d+)}/g, function(match, number) { 
+      //return typeof args[number] != 'undefined'
+        //? args[number]
+        //: match
+      //;
+    //});
+  //};
+//}
 
 //$(function() {
     //var resizebody = function() {
@@ -21,3 +21,12 @@ if (!String.prototype.format) {
     //resizebody();
     //$(window).bind('resize', resizebody);
 //});
+
+$(function() {
+    var url = location.href.substr(0, location.href.length - location.hash.length);
+    $('#nav a').each(function() {
+        if (this.href == url) {
+            $(this).addClass('current');
+        }
+    });
+});
